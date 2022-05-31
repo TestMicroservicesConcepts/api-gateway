3b7c638e5c5fada716746ccafa3f6bc5bc6b3cac
Cristian Camilo Cardozo Moreno <cristian.cardozo@globant.com>
2022-05-31 15:38:24 -0500

    commit inicial. Tutorial completado

---
+++ src/test/java/com/camcar/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camcar.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
